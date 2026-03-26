2e119f4a7b19f8caf273c3d9f4adb984e1176626
syioa <lalitsingh.micro@gmail.com>
2026-03-26 13:19:55 +0530

    refactor: removed unnecessary function inside `exit_gracefully` function

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,4 @@
 export async function exit_gracefully(server: Bun.Server<undefined>) {
-    async function shutdown() {
-        await server.stop();
-        if (process.stdin.isTTY) {
-            process.stdin.setRawMode(false);
-            process.stdin.pause();
-        }
-        process.exit(0);
-    }
-
-
     if (process.stdin.isTTY) {
         process.stdin.setRawMode(true);
 
@@ -17,7 +7,14 @@ export async function exit_gracefully(server: Bun.Server<undefined>) {
 
         process.stdin.on("data", async (key: string) => {
             if (key.toLowerCase() == 'q') {
-                shutdown();
+                await server.stop();
+
+                if (process.stdin.isTTY) {
+                    process.stdin.setRawMode(false);
+                    process.stdin.pause();
+                }
+
+                process.exit(0);
             }
         })
     }
